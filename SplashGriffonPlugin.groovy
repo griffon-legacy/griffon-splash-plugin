96e57aa45a31c5e123447518e383c973142ff585
aalmiray <aalmiray@gmail.com>
2012-03-17 21:12:40 +0100

    Updates for Griffon 0.9.5

--- SplashGriffonPlugin.groovy
+++ SplashGriffonPlugin.groovy
@@ -18,18 +18,65 @@
  * @author Jim Shingler
  */
 class SplashGriffonPlugin {
-    def version = '0.2'
-    def toolkits = ['swing']
-    def griffonVersion = '0.9.2 > *'
-    def license = 'Apache Software License 2.0'
+    // the plugin version
+    String version = '0.3'
+    // the version or versions of Griffon the plugin is designed for
+    String griffonVersion = '0.9.5 > *'
+    // the other plugins this plugin depends on
+    Map dependsOn = [swing: '0.9.5']
+    // resources that are included in plugin packaging
+    List pluginIncludes = []
+    // the plugin license
+    String license = 'Apache Software License 2.0'
+    // Toolkit compatibility. No value means compatible with all
+    // Valid values are: swing, javafx, swt, pivot, gtk
+    List toolkits = ['swing']
+    // Platform compatibility. No value means compatible with all
+    // Valid values are:
+    // linux, linux64, windows, windows64, macosx, macosx64, solaris
+    List platforms = []
+    // URL where documentation can be found
+    String documentation = ''
+    // URL where source can be found
+    String source = 'https://github.com/griffon/griffon-splash-plugin'
 
-    def author = "Jim Shingler"
-    def authorEmail = "ShinglerJim@gmail.com"
-    def title = "A Griffon Splash Screen Plugin"
-    def description = '''
-A Griffon Splash Screen Plugin
+    List authors = [
+        [
+            name: 'Jim Shingler',
+            email: 'ShinglerJim@gmail.com'
+        ],
+        [
+            name: 'Andres Almiray',
+            email: 'aalmiray@yahoo.com'
+        ]
+    ]
+    String title = "Startup splash screen"
+    // accepts Markdown syntax. See http://daringfireball.net/projects/markdown/ for details
+    String description = '''
+Provides a splash screen that is displayed during initialization of Griffon. The plugin provides the for a custom image and 
+status text.
+
+Usage
+-----
+
+The splash screen can be displayed by calling
+
+        SplashGriffonAddon.display(app) 
+
+This code is usually placed inside `griffon-app/lifecycle/Initialize.groovy` as that's the lifecycle script that will be called
+first when the application startups.
+
+### Setting an Image
+
+You can configure a diffrent image to show inside `griffon-app/conf/Config.groovy`
+
+        splash.image = 'mysplash.jpg'
+
+### Updating Status
+
+Updating the status text can be done at anytime while the Splash screen is shown, like this
+
+    griffon.plugins.splash.SplashScreen.instance.showStatus("Initializing the Controller")
 '''
-
-    // URL to the plugin's documentation
-    def documentation = "http://griffon.codehaus.org/Splash+Plugin"
 }
+
